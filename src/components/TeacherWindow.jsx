55456184e6621f1e97ea4f78b0ee76e2272d9dfe
Tushar Ravindra Borse <tushar_borse_it@moderncoe.edu.in>
2025-08-30 11:22:18 +0530

    Add TeacherWindow component

---
+++ src/components/TeacherWindow.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TeacherWindow = () => {
+  return (
+    <div>
+      <h1>Teacher Dashboard</h1>
+      {/* Add teacher-specific features here */}
+    </div>
+  );
+};
+
+export default TeacherWindow;
