ae1394c1d3c3d7fb8454d9d3a91b3a24b631db79
Tushar Ravindra Borse <tushar_borse_it@moderncoe.edu.in>
2025-08-30 11:22:20 +0530

    Create StudentWindow component

---
+++ src/components/StudentWindow.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StudentWindow = () => {
+  return (
+    <div>
+      <h1>Student Dashboard</h1>
+      {/* Add student-specific features here */}
+    </div>
+  );
+};
+
+export default StudentWindow;
